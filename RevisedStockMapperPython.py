c83a1d576849527908ea5ead735b0c29f74b6d5e
codecse <105579511+codecse@users.noreply.github.com>
2024-02-16 14:14:31 +0530

    Update RevisedStockMapperPython.py

--- RevisedStockMapperPython.py
+++ RevisedStockMapperPython.py
@@ -1,20 +1,18 @@
-filename = "output.txt"
-max_close_prices = {}
+input_file_path = "StockDataSet.txt"
+output_file_path = "output.txt"
 
-with open(filename, 'r') as file:
-    for line in file:
-        parts = line.strip().split(" ")
+input_file = open(input_file_path, 'r')
+output_file = open(output_file_path, 'w')
 
-        if len(parts) >= 3:
-            key = parts[0] + "," + parts[1]
-            try:
-                close_price = float(parts[2])
-            except ValueError:
-                # Handle non-float values gracefully
-                continue
-
-            if key not in max_close_prices or close_price > max_close_prices[key]:
-                max_close_prices[key] = close_price
-
-for key, value in max_close_prices.iteritems():
-    print key + "\t" + str(value)
+try:
+    for line in input_file:
+        items = line.strip().split(",")
+        if len(items) >= 8:
+            stock_name = items[0]
+            key = items[1]
+            price = float(items[7])
+            output_file.write("{0} {1} {2}\n".format(stock_name, key, price))
+            print("{0} {1} {2}".format(stock_name, key, price))
+finally:
+    input_file.close()
+    output_file.close()
